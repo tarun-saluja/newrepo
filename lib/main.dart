1117727f6981a7368b2b2c292acfe7b2407d2360
kumar.gaurav <kumar.gaurav@hashedin.com>
2019-03-06 12:38:20 +0530

    Clearing master

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:memob/searchbar.dart';
-void main() {
-  runApp(MyApp());
-} 
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SearchBar() 
-       );
-  }
-}
 
 
