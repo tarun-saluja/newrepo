1ab973ce47ca14b69873befc7e6b0631f0560761
kumar.gaurav <kumar.gaurav@hashedin.com>
2019-03-06 15:36:54 +0530

    Data rendering reduced in dashboard

--- lib/allMeetings.dart
+++ lib/allMeetings.dart
@@ -3,18 +3,12 @@ import 'package:flutter/material.dart';
 import './meetingClass.dart';
 import './meetings.dart';
 
-import 'dart:convert';
-import 'dart:io';
-import 'package:connectivity/connectivity.dart';
 import 'package:memob/meetingClass.dart';
-import 'dart:async';
-import 'package:http/http.dart' as http;
-import 'package:memob/utilities.dart' as utilities;
 
 class AllMeetings extends StatefulWidget {
-  final MeetingClass startingMeeting;
+  final List<MeetingClass> meetings;
 
-  AllMeetings({this.startingMeeting});
+  AllMeetings([this.meetings = const []]);
 
   @override
   State<StatefulWidget> createState() {
@@ -23,116 +17,15 @@ class AllMeetings extends StatefulWidget {
 }
 
 class _AllMeetingsState extends State<AllMeetings> {
-   String userToken;
-  bool _connectionStatus = false;
-  final Connectivity _connectivity = new Connectivity();
-
-  List<MeetingClass> _meetings = new List();
-
-  bool meetingDataLoaded = false;
-  bool noteDataLoaded = false;
-
-  var finalDateTime;
-
-  // Map<String, dynamic> data;
-
-  Future<bool> initConnectivity() async {
-    var connectionStatus;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      connectionStatus = (await _connectivity.checkConnectivity());
-
-      this.setState(() {
-        if (connectionStatus == ConnectivityResult.none) {
-          _connectionStatus = false;
-        } else {
-          _connectionStatus = true;
-        }
-      });
-    }
-    catch(PlatformException) {
-      _connectionStatus = false;
-    }
-    if (!mounted) {
-      return false;
-    }
-    return _connectionStatus;
-  }
-
-  Future<Null> fetchData() async {
-    Future<String> token = utilities.getTokenData();
-    token.then((value) {
-      if (value != null) {
-        userToken = value;
-        getMeetingData();
-           if (meetingDataLoaded && noteDataLoaded) return null;
-      }
-       else {
-        utilities.showLongToast(value);
-        return null;
-      }
-    });
-  }
-
-  Future<Null> getMeetingData() async {
-    final response = await http.get(
-        Uri.encodeFull('https://app.meetnotes.co/api/v2/meetings/'),
-        headers: {
-          HttpHeaders.AUTHORIZATION: 'Token $userToken',
-          HttpHeaders.CONTENT_TYPE: 'application/json',
-          HttpHeaders.ACCEPT: 'application/json',
-          HttpHeaders.CACHE_CONTROL: 'no-cache'
-        });
-
-    if (response.statusCode == 200) {
-      this.setState(() {
-        Map<String, dynamic> mData = json.decode(response.body);
-        List<String> list = mData.keys.toList();
-        list.sort();
-
-        Iterable<String> reversedList = list.reversed;
-
-        print(reversedList);
-
-        _meetings = new List();
-
-        for (String keys in reversedList) {
-          List list = mData[keys];
-          for (var meetingData in list) {
-            MeetingClass meeting = new MeetingClass(
-                meetingData['uuid'],
-                meetingData['title'],
-                meetingData['start_time'],
-                meetingData['end_time'],
-                meetingData['event_uuid'],
-                meetingData['has_notes'],
-                meetingData['is_archived']);
-            _meetings.add(meeting);
-          }
-        }
-        meetingDataLoaded = true;
-      });
-      return null;
-    } else {
-      // If that response was not OK, throw an error.
-      meetingDataLoaded = true;
-      return null;
-    }
-  }
+  List<MeetingClass> _meetings;
 
   @override
-  initState() {
+  void initState() {
+    if(widget.meetings!=null){
+      _meetings=widget.meetings;
+    }
     super.initState();
-    initConnectivity().then((result) {
-      if (result) {
-        this.fetchData();
-      } else {
-        meetingDataLoaded = true;
-        noteDataLoaded = true;
-      }
-    });
   }
-
   @override
   Widget build(BuildContext context) {
     return Column(
--- lib/recentlyUpdated.dart
+++ lib/recentlyUpdated.dart
@@ -9,9 +9,9 @@ import 'package:http/http.dart' as http;
 import './notes.dart';
 
 class RecentlyUpdated extends StatefulWidget {
-  NotesClass startingNote;
+   final List<NotesClass> notes;
 
-  RecentlyUpdated({this.startingNote});
+  RecentlyUpdated([this.notes = const []]);
 
   @override
   State<StatefulWidget> createState() {
@@ -20,103 +20,13 @@ class RecentlyUpdated extends StatefulWidget {
 }
 
 class _RecentlyUpdatedState extends State<RecentlyUpdated> {
-   String userToken;
-  bool _connectionStatus = false;
-  final Connectivity _connectivity = new Connectivity();
-
-  List<NotesClass> _notes = new List();
-
-  bool meetingDataLoaded = false;
-  bool noteDataLoaded = false;
-
-  var finalDateTime;
-
-  // Map<String, dynamic> data;
-
-  Future<bool> initConnectivity() async {
-    var connectionStatus;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      connectionStatus = (await _connectivity.checkConnectivity());
-
-      this.setState(() {
-        if (connectionStatus == ConnectivityResult.none) {
-          _connectionStatus = false;
-        } else {
-          _connectionStatus = true;
-        }
-      });
-    }
-    catch(PlatformException) {
-      _connectionStatus = false;
-    }
-    if (!mounted) {
-      return false;
-    }
-    return _connectionStatus;
-  }
-
-  Future<Null> fetchData() async {
-    Future<String> token = utilities.getTokenData();
-    token.then((value) {
-      if (value != null) {
-        userToken = value;
-        getRecentNotes();
-           if (meetingDataLoaded && noteDataLoaded) return null;
-      }
-       else {
-        utilities.showLongToast(value);
-        return null;
-      }
-    });
-  }
-
- Future<Null> getRecentNotes() async {
-    final response = await http.get(
-        Uri.encodeFull('https://app.meetnotes.co/api/v2/recent-notes/'),
-        headers: {
-          HttpHeaders.AUTHORIZATION: 'Token $userToken',
-          HttpHeaders.CONTENT_TYPE: 'application/json',
-          HttpHeaders.ACCEPT: 'application/json',
-          HttpHeaders.CACHE_CONTROL: 'no-cache'
-        });
-
-    if (response.statusCode == 200) {
-      this.setState(() {
-        List<dynamic> mData = json.decode(response.body);
-
-        _notes = new List();
-        for (var recentNote in mData) {
-          NotesClass note = new NotesClass(
-              recentNote['action_items'],
-              recentNote['meeting_title'],
-              recentNote['is_archived'],
-              recentNote['updated_at'],
-              recentNote['meeting_uuid'],
-              recentNote['event_uuid']);
-          _notes.add(note);
-        }
-        noteDataLoaded = true;
-      });
-      return null;
-    } else {
-      // If that response was not OK, throw an error.
-      noteDataLoaded = true;
-      return null;
-    }
-  }
-
+List<NotesClass> _notes;
   @override
-  initState() {
+  void initState() {
+    if(widget.notes!=null){
+      _notes=widget.notes;
+    }
     super.initState();
-    initConnectivity().then((result) {
-      if (result) {
-        this.fetchData();
-      } else {
-        meetingDataLoaded = true;
-        noteDataLoaded = true;
-      }
-    });
   }
   @override
   Widget build(BuildContext context) {
